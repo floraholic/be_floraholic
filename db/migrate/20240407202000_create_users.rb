cc232eca8cc0575331a372eb40b044b6c05f4086
Matt Darlington <matthewsdarlington@gmail.com>
2024-04-07 15:11:35 -0600

    Remove unwanted '_string' after l_name and e_mail in initial create_users migration

--- db/migrate/20240407202000_create_users.rb
+++ db/migrate/20240407202000_create_users.rb
@@ -2,8 +2,8 @@ class CreateUsers < ActiveRecord::Migration[7.1]
   def change
     create_table :users do |t|
       t.string :f_name
-      t.string :l_name_string
-      t.string :email_string
+      t.string :l_name
+      t.string :email
       t.string :phone
       t.string :street
       t.string :city
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_07_194212) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_07_202320) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -24,8 +24,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_04_07_194212) do
 
   create_table "users", force: :cascade do |t|
     t.string "f_name"
-    t.string "l_name_string"
-    t.string "email_string"
+    t.string "l_name"
+    t.string "email"
     t.string "phone"
     t.string "street"
     t.string "city"
